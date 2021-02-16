139e42c19d13f4a23d6497dc4844425214412980
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-16 13:39:27 +0600

    added some code in app.js file

--- analyze/app.js
+++ analyze/app.js
@@ -1,1 +1,7 @@
-document.title = 'play with DOM';
+//  get element by id
+// const newTask = document.getElementById('new-task');
+// const header = document.getElementById('header');
+// header.innerText = 'My Todo List';
+// header.innerHTML = '<h1>hello</h1>';
+// header.textContent = 'my todo list';
+console.dir(document);
