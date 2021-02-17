263321abaf9a4fea5e0d1ad598e64cc19259bb61
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-17 22:32:13 +0600

    completed completed task part

--- project/app.js
+++ project/app.js
@@ -13,10 +13,30 @@ const createTask = (task) => {
   listItem.appendChild(label);
 
   listItems.append(listItem);
+  checkBox.addEventListener('click', function () {
+    completedTask(task);
+  });
 };
 
 const addTask = () => {
   let inputValue = document.getElementById('new-task');
   createTask(inputValue.value);
   inputValue.value = '';
+};
+
+const completedTask = (task) => {
+  const completedList = document.getElementById('completed-task');
+  const listItem = document.createElement('li');
+  const label = document.createElement('label');
+  const button = document.createElement('button');
+  button.classList = 'delete';
+
+  label.innerText = task;
+  button.type = 'button';
+  button.innerText = 'delete';
+
+  listItem.appendChild(label);
+  listItem.appendChild(button);
+
+  completedList.append(listItem);
 };
